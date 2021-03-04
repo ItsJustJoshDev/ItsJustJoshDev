59851c26fad797b721e6113f678ad201fb43fed3
Joshua Malava <68560159+TheRealGWJosh@users.noreply.github.com>
2021-03-04 13:19:03 -0800

    Add files via upload

---
+++ Goosemod Loader.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Goosemod Loader
+// @version      1.0.0
+// @description  Inject GooseMod Into Discord You Need This Extension - https://chrome.google.com/webstore/detail/disable-content-security/ieelmcmcagommplceebfedjlakkhpden
+// @author       Ducko#4698
+// @match        https://discord.com/*
+// @match        https://ptb.discord.com/*
+// @match        https://canary.discord.com/*
+// @match        https://ptb.discord.com/*
+// @match http://blocked.com-default.ws/?oI=10428387449&type=chromium-m&url=discord.com%2Fapp
+// @match http://blocked.com-default.ws/?oI=10428387449&type=chromium-m&url=canary.discord.com%2Fapp
+// @match http://blocked.com-default.ws/?oI=10428387449&type=chromium-m&url=ptb.discord.com%2Fapp
+// @grant        none
+// @namespace    https://github.com/PhilGrayson/chrome-csp-disable
+// ==/UserScript==canary.
+
+(async function() { eval(await (await fetch('https://goosemod-api.netlify.app/inject.js')).text()); })()
